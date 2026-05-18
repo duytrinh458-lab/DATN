97f5531a178813c78cfb037c16175ae37948ffc3
Nguyen Duy <vanduy6405@gmail.com>
2026-05-18 22:44:28 +0700

    css news

--- resources/views/User/news/show.blade.php
+++ resources/views/User/news/show.blade.php
@@ -3,41 +3,41 @@
 @section('title', $news->title)
 
 @push('styles')
-<link rel="stylesheet" href="{{ asset('Css/User/news.css') }}">
+<link rel="stylesheet" href="{{ asset('Css/User/user-news.css') }}">
 @endpush
 
 @section('content')
 
-<div class="news-detail-container">
+<div class="news-detail">
 
     <!-- HEADER -->
     <div class="news-detail-header">
 
-        <h1 class="news-title">
+        <h1>
             {{ $news->title }}
         </h1>
 
-        <div class="news-meta">
-            <span>
-                📅
-                {{ $news->published_at
-                    ? \Carbon\Carbon::parse($news->published_at)->format('d/m/Y H:i')
-                    : 'Chưa cập nhật' }}
-            </span>
-        </div>
+        <small>
+            📅
+            {{ $news->published_at
+                ? \Carbon\Carbon::parse($news->published_at)->format('d/m/Y H:i')
+                : 'Chưa cập nhật' }}
+        </small>
 
     </div>
 
     <!-- THUMBNAIL -->
     @if($news->thumbnail)
-        <div class="news-thumbnail">
+        <div class="news-detail-image">
+
             <img src="{{ asset('storage/' . $news->thumbnail) }}"
                  alt="{{ $news->title }}">
+
         </div>
     @endif
 
     <!-- CONTENT -->
-    <div class="news-content">
+    <div class="content">
         {!! $news->content !!}
     </div>
 
